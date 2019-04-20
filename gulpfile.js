7a39eca7097bab3fbca5f76ec98f11352ec21d83
Patrick Whitehouse <patrick1996@gmail.com>
2019-04-20 18:20:51 +0100

    add css minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
-const purify = require('gulp-purifycss');
 const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
 const browserSync = require('browser-sync').create();
 
 
@@ -39,4 +40,18 @@ gulp.task('browser-sync', function() {
 
     gulp.watch("./**/*.html").on('change', browserSync.reload);
     gulp.watch("scss/**/*.scss", ['sass']);
+});
+
+
+/* Minify CSS */
+gulp.task('minify-css', function() {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({
+            compatibility: '*',
+            level: '1'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
 });
